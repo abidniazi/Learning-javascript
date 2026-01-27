3c85427e82dc0469760948fef32b79b5fae68519
Muhammad abid <abidniazi3443@gmail.com>
2026-01-27 19:14:30 +0500

    practice new question

--- asynch.js
+++ asynch.js
@@ -89,3 +89,25 @@
 // }
 // getuser()
 
+
+
+// function
+
+async function getData() {
+  const url = "https://example.org/products.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+ getData()
+
+
