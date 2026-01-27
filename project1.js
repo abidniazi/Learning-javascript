20426903b53db6c4eb2fe36e3c84970cf6d68d53
Muhammad abid <abidniazi3443@gmail.com>
2026-01-27 23:19:09 +0500

    Gsap project added

---
+++ project1.js
@@ -0,0 +1,31 @@
+console.log("aa")
+
+var rect=document.querySelector(".box")
+
+rect.addEventListener("mousemove",function(details){
+   var rectanglelocation=rect.getBoundingClientRect();
+   var insiderectangle=details.clientX - rectanglelocation.left
+   if(insiderectangle<rectanglelocation.width/2){
+
+    var redcolor= gsap.utils.mapRange(0,rectanglelocation.width/2,255,0,insiderectangle);
+    gsap.to(rect,{
+        backgroundColor: `rgb(${redcolor},0,0)`,ease:
+        Power4,
+    })
+
+   }else{
+       var bluecolor= gsap.utils.mapRange(rectanglelocation.width/2,rectanglelocation.width,0,255,insiderectangle);
+    gsap.to(rect,{
+        backgroundColor: `rgb(0,0,${bluecolor})`,ease:
+        Power4,
+    })
+
+   }
+
+})
+
+rect.addEventListener("mouseleave",function(){
+    gsap.to(rect,{
+        backgroundColor:"white"
+    })
+})
